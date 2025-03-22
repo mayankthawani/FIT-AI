7681afae6fe6a9846311fbf7308b39d16fd6fc29
sakshambajpai1604 <sakshambajpai.1604@gmail.com>
2025-03-23 00:08:51 +0530

    updated the head rotation pose detection

--- Pose-Detection/utils/headrotation.js
+++ Pose-Detection/utils/headrotation.js
@@ -2,8 +2,8 @@ import { getAngle } from "./poseUtils";
 
 export const detectHeadRotation = (keypoints) => {
     const nose = keypoints[0];
-    const leftear = keypoints[3];
-    const rightear = keypoints[4];
+    const leftear = keypoints[7];
+    const rightear = keypoints[8];
 
     const headRotationAngle = getAngle(nose, leftear, rightear);
     return (headRotationAngle > 45) ? "head rotation" : null;

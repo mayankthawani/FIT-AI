81b815af049cc89fa470503bd2e899d736c386a3
sakshambajpai1604 <sakshambajpai.1604@gmail.com>
2025-03-22 14:30:25 +0530

    added pushup pose detection

---
+++ Pose-Detection/utils/pushup.js
@@ -0,0 +1,18 @@
+import { getAngle } from "./poseUtils";
+
+export const detectPushup = (keypoints) => {
+    const leftshoulder = keypoints[11];
+    const rightshoulder = keypoints[12];
+    const leftelbow = keypoints[13];
+    const rightelbow = keypoints[14];
+    const leftwrist = keypoints[15];
+    const rightwrist = keypoints[16];
+  
+    const avgshoulder = (leftshoulder + rightshoulder) / 2;
+    const avgelbow = (leftelbow + rightelbow) / 2;
+    const avgwrist = (leftwrist + rightwrist) / 2;
+  
+    // ✅ If knees are significantly lower than hips, it's a squat
+    const angle = getAngle(avgshoulder, avgelbow, avgwrist);
+    return (angle < 90) ? "pushdown" : "pushup";
+  };
